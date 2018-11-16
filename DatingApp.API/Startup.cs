507efaf032632c6f1dacdff82610a3838e8cd7a7
Amine TOUCHENE <atouchene@groupeastek.fr>
2018-11-16 17:33:27 +0100

    Initial Commit

---
+++ DatingApp.API/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Dependency injection +++
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x=>x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Check the envirement, if dev mode +++
+            if (env.IsDevelopment())
+            {
+                // Then use pages in browser for repporting the exceptions etc +++ 
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // else for security enhancement 
+                //app.UseHsts(); // dont worry about security 
+            }
+
+            //app.UseHttpsRedirection(); // dont worry, see this later 
+            // Our Framework, it gives the ability to route to the differentes Actions etc +++
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseMvc(); // This is a midelware beetwen the client and the end point = api
+        }
+    }
+}
